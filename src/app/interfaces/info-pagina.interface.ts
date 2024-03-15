b3bad27898501b357bbb273941d61049ef8be925
Yandira Diaz <yandiradiaz@hotmail.com>
2024-03-14 21:12:32 -0300

    cambios en interface info-pagina

--- src/app/interfaces/info-pagina.interface.ts
+++ src/app/interfaces/info-pagina.interface.ts
@@ -1,13 +1,13 @@
 export interface InfoPagina {
-  titulo: string;
-  email: string;
-  nombre_corto: string;
-  pagina_autor: string;
-  facebook: string;
-  twitter: string;
-  instagram: string;
-  tumblr: string;
-  equipo_trabajo: any[];
+  titulo?: string; // el signo ? indica que son opcionales, puede ser que venga o que no venga
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
 }
 
 
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
 
   constructor( private http: HttpClient){
@@ -15,7 +16,7 @@ export class InfoPaginaService {
 
       // Para leer el archivo JSON y tomar sus propiedades para que pueda ser utilizado en las páginas. Necesito un módulo que me permita hacer peticiones HTTP
       this.http.get('assets/data/data-pagina.json')
-        .subscribe (resp => {
+        .subscribe ((resp: InfoPagina) => {
 
           this.cargada = true;
           this.info = resp;
