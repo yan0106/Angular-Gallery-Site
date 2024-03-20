f38050945390dbb4f620a64cc58f8ffae01f99cc
Yandira Diaz <yandiradiaz@hotmail.com>
2024-03-20 18:24:07 -0300

    se envían parámetros por URL

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ItemComponent {
 
+  constructor ( private route: ActivatedRoute){}
+
+  ngOnInit(){
+
+    this.route.params
+    .subscribe(parametros =>{ // el suscribe va a estar pendiente de todos los cambios que sucedan con los parámetros de la URL
+
+      console.log(parametros['id']);
+
+    })
+}
 }
